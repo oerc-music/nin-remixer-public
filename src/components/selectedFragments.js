a871517574e577ad068d059d8430c88c9abc6e7c
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2018-05-17 15:57:09 +0100

    don't replicate selectedFragments col

--- src/components/selectedFragments.js
+++ src/components/selectedFragments.js
@@ -8,23 +8,14 @@ const FragList = BareFragList
 export var FragmentSelector = function({dispatch, selectedFrags, frags, svg}) {
   const selectOnClick = (index, id)=> {dispatch(selectFragment(index, id))}
   return (
-      <div>
-        { 
-          [...selectedFrags.entries()].map(([ind,item])=>{
-            //<p> Selection (flist.id) </p>
-
-            return (
-            <div className="listcol">
-							<p> Selection </p>
-							<FragList fragments={frags}
-												svg={svg}
-												onClick={selectOnClick} />
-							<hr/>
-            </div>
-            )
-          })}
-      </div>
-    )
+	<div className="listcol">
+					<p> Selection </p>
+					<FragList fragments={frags}
+										svg={svg}
+										onClick={selectOnClick} />
+					<hr/>
+	</div>
+  )
 }
 FragmentSelector = connect(s=>s)(FragmentSelector)
 
